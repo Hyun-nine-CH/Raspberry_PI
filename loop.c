c7d1a7514662b42445d5992a19214ef188a08c77
Hyun-nine-CH <ttmstym234@gmail.com>
2025-06-12 17:02:47 +0900

    Thursday_5

--- loop.c
+++ loop.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+
+void sigHd1(int no) {
+    printf("%d\n", no);
+}
 
 int main(int argc, char **argv) {
     int i;
+    signal(SIGINT, sigHd1);
     for(i=0; ; i++) {
         printf("%10d\r", i);
         fflush(NULL);
